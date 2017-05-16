65cf6ff2a1be811b9f11960aec506d7f4ba646b7
Georg Derflinger <georgderflinger@gmail.com>
2017-05-16 13:55:36 +0200

    work on telegramm builder

---
+++ TelegrammBuilder/ITelegrammBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegrammBuilder
+{
+    interface ITelegrammBuilder
+    {
+       void GotIoChange( int index, bool value );
+       event NewTelegramm ENewTelegramm;
+    }
+}
---
+++ TelegrammBuilder/TelegrammBuilder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemServices;
+
+namespace TelegrammBuilder
+{
+    public class TelegrammEventArgs : EventArgs
+    {
+        string MsgTelegramm;
+    }
+
+    public static class FurnitureStatus
+    {
+        public static string Open   = "OPEN";
+        public static string Closed = "CLOSED";
+    }
+
+    public class Furniture
+    {
+        public string Name { get; set; }
+        public string TimeStampWhenStatusChange { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class Telegramm
+    {
+        public string Room { get; set; }
+        public Dictionary<int, Furniture> FurnitureStatusInformation { get; set; }
+    }
+
+    public delegate void NewTelegramm( object sender, TelegrammEventArgs e );
+
+    public class TelegrammBuilder : ITelegrammBuilder
+    {
+        ITimeUtil _TimeStamp;
+        public TelegrammBuilder( ITimeUtil TimeStamp )
+        {
+
+        }
+        public event NewTelegramm ENewTelegramm;
+        public void GotIoChange( int index, bool value )
+        {
+        }
+    }
+}
