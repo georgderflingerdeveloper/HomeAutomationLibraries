8487c44ed1f377d27e5fdf93052db0fc69fcec39
georgderflingerdeveloper <georgderflinger@gmail.com>
2017-05-16 21:34:41 +0200

    unit test template for telegramm builder

---
+++ HomeAutomationLibraries-master/TelegrammBuilder/UNIT_TESTS/TelegrammBuilder_UnitTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace TELEGRAMM_BUILDER_UNIT_TESTS
+{
+    [TestFixture( )]
+    public class Test
+    {
+        [Test( )]
+        public void TestCase( )
+        {
+        }
+    }
+}
