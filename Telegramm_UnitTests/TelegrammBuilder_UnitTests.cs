2b5d78df20a86e525dc6b7f724c6109ea15d2ae8
Georg Derflinger <georgderflinger@gmail.com>
2017-05-17 18:35:35 +0200

    template for telegram builder unit tests

---
+++ Telegramm_UnitTests/TelegrammBuilder_UnitTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using TelegrammBuilder;
+using System;
+using Moq;
+
+namespace Telegramm_UnitTests
+{
+    [TestFixture]
+    public class TelegrammBuilder_UnitTests
+    {
+    }
+}
