cf4a658be184bcb51d16ed9702bc8313b7d13708
georgderflingerdeveloper <georgderflinger@gmail.com>
2017-05-21 16:30:06 +0200

    do a sync

---
+++ HomeAutomationLibraries-master/TelegrammBuilder/TelegrammBuilder.cs
@@ -0,0 +1,147 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using SystemServices;
+using HardConfig.SLEEPINGROOM;
+using HardConfig.COMMON;
+
+namespace TelegrammBuilder
+{
+    public class TelegrammEventArgs : EventArgs
+    {
+        public string MsgTelegramm { get; set; }
+    }
+
+    public static class DeviceStatus
+    {
+        public static string Open       = "OFFEN";
+        public static string Closed     = "GESCHLOSSEN";
+        public static string TippedOver = "GEKIPPT";
+        public static string Unknown    = "UNKNOWN";
+    }
+
+    public static class TelegrammStatus
+    {
+        public static string TelegramConfigurationMismatch = "Mismatch in telegram configuration !";
+    }
+
+    public class Device
+    {
+        public string Name { get; set; }
+        public string TimeStampWhenStatusChange { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class Telegramm
+    {
+        public string Room { get; set; }
+        public Dictionary<int, Device> FurnitureStatusInformation { get; set; }
+    }
+
+    public class SleepingRoomTelegramm : Telegramm
+    {
+        public SleepingRoomTelegramm()
+        {
+            Room = SleepingRoomDeviceNames.RoomName;
+            FurnitureStatusInformation = new Dictionary<int, Device>
+            {
+                { SleepingRoomIODeviceIndices.indDigitalInputWindowWest,
+                    new Device{
+                                   Name                      = SleepingRoomDeviceNames.WindowWest_,
+                                   Status                    = DeviceStatus.Unknown,
+                                   TimeStampWhenStatusChange = GeneralConstants.EmptyTimeStamp
+                                 }
+                },
+                { SleepingRoomIODeviceIndices.indDigitalInputMansardWindowNorthLeft,
+                    new Device{
+                                   Name                      = SleepingRoomDeviceNames.MansardWindowNorthLeft,
+                                   Status                    = DeviceStatus.Unknown,
+                                   TimeStampWhenStatusChange = GeneralConstants.EmptyTimeStamp
+                                 }
+                }
+            };
+        }
+    }
+
+    public delegate void NewTelegramm( object sender, TelegrammEventArgs e );
+
+    public class MsgTelegrammBuilder : ITelegrammBuilder
+    {
+        ITimeUtil _TimeStamp;
+        TelegrammEventArgs TelEventArgs = new TelegrammEventArgs( );
+        Telegramm _TelegrammTemplate;
+        string TelegrammTrailer;
+        string[] TelegrammContainer;
+
+        public MsgTelegrammBuilder( Telegramm TelegrammTemplate, ITimeUtil TimeStamp )
+        {
+            _TelegrammTemplate = TelegrammTemplate;
+            _TimeStamp = TimeStamp;
+            MakeMsgTelegramTemplate( );
+        }
+
+        public event NewTelegramm ENewTelegramm;
+
+        void FireTelegramm( )
+        {
+            ENewTelegramm?.Invoke( this, TelEventArgs );
+        }
+
+        void MakeMsgTelegramTemplate( )
+        {
+            string telegramm = "";
+            int index = 0;
+
+            foreach( KeyValuePair<int, Device> pairs in _TelegrammTemplate.FurnitureStatusInformation )
+            {
+               telegramm += pairs.Value.Name;
+               telegramm += Seperators.TelegrammSeperator;
+               telegramm += pairs.Value.Status;
+               telegramm += Seperators.TelegrammSeperator;
+               telegramm += pairs.Value.TimeStampWhenStatusChange;
+               index++;
+               if( index < _TelegrammTemplate.FurnitureStatusInformation.Count )
+               {
+                   telegramm += Seperators.TelegrammSeperator;
+               }
+            }
+
+            TelegrammContainer = telegramm.Split( Seperators.TelegrammSeperator );
+        }
+
+        void ActualiseTelegramm( int numberAsKey, bool value )
+        {
+            TelegrammTrailer = _TelegrammTemplate.Room;
+            TelEventArgs.MsgTelegramm = null;
+
+            if( _TelegrammTemplate.FurnitureStatusInformation.ContainsKey( numberAsKey ) )
+            {
+                int index = 0;
+                foreach( string elements in TelegrammContainer )
+                {
+                    if( TelegrammContainer[index] == _TelegrammTemplate.FurnitureStatusInformation[numberAsKey].Name )
+                    {
+                        break;
+                    }
+                    index++;
+                }
+
+                TelegrammContainer[index]     = _TelegrammTemplate.FurnitureStatusInformation[numberAsKey].Name;
+                TelegrammContainer[index + 1] = _TelegrammTemplate.FurnitureStatusInformation[numberAsKey].Status = value ? DeviceStatus.Open : DeviceStatus.Closed;
+                TelegrammContainer[index + 2] = _TelegrammTemplate.FurnitureStatusInformation[numberAsKey].TimeStampWhenStatusChange = _TimeStamp.IGetTimeStamp( );
+
+                TelEventArgs.MsgTelegramm = TelegrammTrailer + Seperators.TelegrammSeperator + String.Join( Seperators.TelegrammSeperatorStr, TelegrammContainer );
+            }
+            else
+            {
+                TelEventArgs.MsgTelegramm = TelegrammStatus.TelegramConfigurationMismatch;
+            }
+
+        }
+
+        public void GotIoChange( int ionumber, bool value )
+        {
+            ActualiseTelegramm( ionumber, value );
+            FireTelegramm( );
+        }
+    }
+}
---
+++ HomeAutomationLibraries-master/Telegramm_UnitTests/TelegrammBuilder_UnitTests.cs
@@ -0,0 +1,190 @@
+﻿using NUnit.Framework;
+using TelegrammBuilder;
+using System;
+using Moq;
+using SystemServices;
+using HardConfig.SLEEPINGROOM;
+using HardConfig.COMMON;
+
+namespace Telegramm_UnitTests
+{
+    [TestFixture]
+    public class TelegrammBuilder_UnitTests
+    {
+        Mock<ITimeUtil>       MockTimeStamp;
+        MsgTelegrammBuilder   TestBuilder;
+        SleepingRoomTelegramm TestTelegramm = new SleepingRoomTelegramm( );
+        string[] TestTelgrammElements;
+        string Telegramm;
+
+        int WrongIndexForTesting = 0;
+
+        string TestTimeStamp            = "15052017:20h16m33s520ms";
+        string DifferentTestTimeStamp   = "19122018:21h29m44s333ms";
+        string DifferentTestTimeStamp_2 = "19122018:21h35m44s333ms";
+        string VerificationTelegramm_1  = "Schlafzimmer_FensterWestSeite_OFFEN_15052017:20h16m33s520ms_MansardenFensterLinkeSeite_GESCHLOSSEN_19122018:21h29m44s333ms";
+        string VerificationTelegramm_2  = "Schlafzimmer_FensterWestSeite_GESCHLOSSEN_19122018:21h29m44s333ms_MansardenFensterLinkeSeite_OFFEN_19122018:21h35m44s333ms";
+
+        void SetupMockedTimestamp()
+        {
+            MockTimeStamp = new Mock<ITimeUtil>( );
+            MockTimeStamp.Setup( tobj => tobj.IGetTimeStamp( ) ).Returns( TestTimeStamp );
+        }
+
+        void SetupTelegrammBuilder( )
+        {
+            SetupMockedTimestamp( );
+            TestBuilder = new MsgTelegrammBuilder( TestTelegramm , MockTimeStamp.Object );
+        }
+
+        void CleanUp()
+        {
+            TestTelgrammElements = null;
+        }
+
+        [Test]
+        public void Test_TelegramConfigurationMismatch()
+        {
+            CleanUp( );
+
+            SetupTelegrammBuilder( );
+
+            TestBuilder.ENewTelegramm += ( sender, e ) =>
+            {
+                TestTelgrammElements = e.MsgTelegramm.Split( Seperators.TelegrammSeperator );
+            };
+
+            TestBuilder.GotIoChange( WrongIndexForTesting, false );
+
+            Assert.AreEqual( TelegrammStatus.TelegramConfigurationMismatch, TestTelgrammElements[0] );
+        }
+
+        [Test]
+        public void Test_FirstTelegramm_Element_RoomIdentification()
+        {
+            CleanUp( );
+
+            SetupTelegrammBuilder( );
+
+            TestBuilder.ENewTelegramm += ( sender, e ) =>
+            {
+                TestTelgrammElements = e.MsgTelegramm.Split( Seperators.TelegrammSeperator );
+            };
+
+            TestBuilder.GotIoChange( SleepingRoomIODeviceIndices.indDigitalInputWindowWest, false );
+
+            Assert.AreEqual( SleepingRoomDeviceNames.RoomName, TestTelgrammElements[0] );
+        }
+
+        [Test]
+        public void Test_NextTelegramm_Element_FurnitureType()
+        {
+            CleanUp( );
+
+            SetupTelegrammBuilder( );
+
+            TestBuilder.ENewTelegramm += ( sender, e ) =>
+            {
+                TestTelgrammElements = e.MsgTelegramm.Split( Seperators.TelegrammSeperator );
+            };
+
+            TestBuilder.GotIoChange( SleepingRoomIODeviceIndices.indDigitalInputWindowWest, false );
+
+            Assert.AreEqual( SleepingRoomDeviceNames.WindowWest_, TestTelgrammElements[1] );
+        }
+
+        [Test]
+        public void Test_NextTelegramm_Element_TimeStamp()
+        {
+            CleanUp( );
+
+            SetupTelegrammBuilder( );
+
+            TestBuilder.ENewTelegramm += ( sender, e ) =>
+            {
+                TestTelgrammElements = e.MsgTelegramm.Split( Seperators.TelegrammSeperator );
+            };
+
+            TestBuilder.GotIoChange( SleepingRoomIODeviceIndices.indDigitalInputWindowWest, false );
+
+            Assert.AreEqual( TestTimeStamp, TestTelgrammElements[3] );
+        }
+
+        [Test]
+        public void Test_NextTelegramm_Element_StatusIsClosed()
+        {
+            CleanUp( );
+
+            SetupTelegrammBuilder( );
+
+            TestBuilder.ENewTelegramm += ( sender, e ) =>
+            {
+                TestTelgrammElements = e.MsgTelegramm.Split( Seperators.TelegrammSeperator );
+            };
+
+            TestBuilder.GotIoChange( SleepingRoomIODeviceIndices.indDigitalInputWindowWest, false );
+
+            Assert.AreEqual( DeviceStatus.Closed, TestTelgrammElements[2] );
+        }
+
+        [Test]
+        public void Test_NextTelegramm_Element_StatusIsOpen()
+        {
+            CleanUp( );
+
+            SetupTelegrammBuilder( );
+
+            TestBuilder.ENewTelegramm += ( sender, e ) =>
+            {
+                TestTelgrammElements = e.MsgTelegramm.Split( Seperators.TelegrammSeperator );
+            };
+
+            TestBuilder.GotIoChange( SleepingRoomIODeviceIndices.indDigitalInputWindowWest, true  );
+
+            Assert.AreEqual( DeviceStatus.Open, TestTelgrammElements[2] );
+        }
+
+        [Test]
+        public void Test_TelegrammGeneration()
+        {
+            CleanUp( );
+
+            SetupTelegrammBuilder( );
+
+            TestBuilder.ENewTelegramm += ( sender, e ) =>
+            {
+                Telegramm = e.MsgTelegramm;
+            };
+
+            TestBuilder.GotIoChange( SleepingRoomIODeviceIndices.indDigitalInputWindowWest, true );
+            MockTimeStamp.Setup( tobj => tobj.IGetTimeStamp( ) ).Returns( DifferentTestTimeStamp );
+            TestBuilder.GotIoChange( SleepingRoomIODeviceIndices.indDigitalInputMansardWindowNorthLeft, false );
+
+            Assert.AreEqual( VerificationTelegramm_1, Telegramm );
+        }
+
+        [Test]
+        public void Test_UpdateDiagramm_Window_Closed_Open_()
+        {
+            CleanUp( );
+
+            SetupTelegrammBuilder( );
+
+            TestBuilder.ENewTelegramm += ( sender, e ) =>
+            {
+                Telegramm = e.MsgTelegramm;
+            };
+
+            TestBuilder.GotIoChange( SleepingRoomIODeviceIndices.indDigitalInputWindowWest, true );
+            MockTimeStamp.Setup( tobj => tobj.IGetTimeStamp( ) ).Returns( DifferentTestTimeStamp );
+            TestBuilder.GotIoChange( SleepingRoomIODeviceIndices.indDigitalInputMansardWindowNorthLeft, false );
+
+            TestBuilder.GotIoChange( SleepingRoomIODeviceIndices.indDigitalInputWindowWest, false );
+            MockTimeStamp.Setup( tobj => tobj.IGetTimeStamp( ) ).Returns( DifferentTestTimeStamp_2 );
+            TestBuilder.GotIoChange( SleepingRoomIODeviceIndices.indDigitalInputMansardWindowNorthLeft, true );
+
+            Assert.AreEqual( VerificationTelegramm_2, Telegramm );
+        }
+
+    }
+}
