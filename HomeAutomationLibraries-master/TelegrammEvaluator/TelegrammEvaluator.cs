eb7473919d33408e674e1f9031c8c424adaa5d6f
georgderflinger <georgderflinger@gmail.com>
2017-05-23 16:53:27 +0200

    work on telegramm brocker

--- HomeAutomationLibraries-master/TelegrammEvaluator/TelegrammEvaluator.cs
+++ HomeAutomationLibraries-master/TelegrammEvaluator/TelegrammEvaluator.cs
@@ -1,39 +1,120 @@
 ﻿using System;
 using TelegrammEvaluator.INTERFACE;
 using HardConfig.COMMON;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TelegrammBuilder;
 
 namespace TelegrammEvaluator
 {
-    public enum EvaluatorState
+ 
+    public enum WindowState 
     {
         eUnknown,
         eOk,
-        eAlarmWindowIsOpen,
-        eAlarmDoorIsOpen
+        eWindowIsOpen
     }
 
-    public class EvaluatorEvents : EventArgs
+    public enum DoorState 
     {
-        public EvaluatorState State { get; set; }
+        eUnknown,
+        eOk,
+        eDoorIsOpen
     }
 
-    public delegate void Informer( object sender, EvaluatorEvents e );
-
-    public class TelegrammEvaluator : ITelegrammEvaluator
+    public class EvaluatorEventArgsWindow : EventArgs
     {
-        EvaluatorState State;
-        string _receivedTelegramm;
+        public WindowState State { get; set; }
+    }
 
-        public TelegrammEvaluator( )
-        {
+    public class EvaluatorEventArgsDoor : EventArgs
+    {
+        public DoorState State { get; set; }
+    }
+    public delegate void Informer( object sender, EventArgs e );
+     
+    public abstract class TelegrammEvaluator : ITelegrammEvaluator
+    {
+        protected string _receivedTelegramm;
+        public virtual string ReceivedTelegramm { get => _receivedTelegramm; set => _receivedTelegramm = value; }
+        public event Informer EInformer;
+    }
+
+    public class WindowTelegrammEvaluator : TelegrammEvaluator
+    {
+        EvaluatorEventArgsWindow WindowArgs = new EvaluatorEventArgsWindow();
+        public delegate void Informer( object sender, EvaluatorEventArgsWindow e );
+
+        public new event Informer EInformer;
+        public override string ReceivedTelegramm 
+        { 
+            set 
+            {
+                _receivedTelegramm  = value;  
+
+            } 
         }
 
+        void Evaluate( string telegramm )
+        {
+            if( telegramm.Contains( TelegrammTypes.WindowInformation ) )
+            {
+                if( telegramm.Contains( DeviceStatus.Open ) )
+                {
+                    WindowArgs.State = WindowState.eWindowIsOpen;
+                    EInformer?.Invoke( this, WindowArgs );
+                }
+            }
+                   
+        }
+    }
 
+    public class DoorTelegrammEvaluator : TelegrammEvaluator
+    {
+        public delegate void Informer( object sender, EvaluatorEventArgsDoor e );
+    }
 
+    public class TelegrammBrocker
+    {
+        
+        public TelegrammBrocker()
+        {
+            foreach( var item in Evaluators )
+            {
+                item.EInformer += (sender, e) => 
+                {
+                    EInformer?.Invoke( sender, e );
+                };
+            }
+        }
 
-
-        public string ReceivedTelegramm { get => _receivedTelegramm; set => _receivedTelegramm = value; }
         public event Informer EInformer;
 
+        List<TelegrammEvaluator> Evaluators = new List<TelegrammEvaluator>()
+        {
+            new WindowTelegrammEvaluator(),
+            new DoorTelegrammEvaluator()
+        };
+
+        public delegate void Informer( object sender, EventArgs e );
+
+        string _receivedTelegramm;
+        public string ReceivedTelegramm 
+        { 
+            set 
+            {
+                _receivedTelegramm  = value;  
+                PassTelegramm( _receivedTelegramm );
+            } 
+        }
+
+        void PassTelegramm( string telegramm )
+        {
+           foreach( var item in Evaluators )
+           {
+                item.ReceivedTelegramm = telegramm;
+           }
+        }
+        
     }
 }
