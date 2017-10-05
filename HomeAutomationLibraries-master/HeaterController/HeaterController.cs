31c9a5a6bb828cdbcad183e37ee8d040bff38e77
Georg Derflinger <georgderflinger@gmail.com>
2017-10-05 16:16:50 +0200

    start with interface definition, status and modi heater control

---
+++ HomeAutomationLibraries-master/HeaterController/HeaterController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeaterController
+{
+    [Serializable]
+    public class HeaterStatus
+    {
+        public enum OperationMode
+        {
+            Permanent,
+            PulseWidthModulation,
+            TemperatureControlHystersis
+        }
+
+        public OperationMode SelectedOperationMode { get; set; }
+
+        public enum ControllerState
+        {
+            ControllerIsOff,
+            ControllerIsOn,
+            ControllerIsPaused
+        }
+
+        public ControllerState ActualControllerState { get; }
+
+        public enum OperationState
+        {
+            RegularOperation,
+            Boosting,
+            Defrosting
+        }
+
+        public OperationState ActualOperationState { get; }
+
+    }
+
+    [Serializable]
+    public class HeaterMode
+    {
+        public enum OperationMode
+        {
+            Permanent,
+            PulseWidthModulation,
+            TemperatureControlHystersis
+        }
+
+        public OperationMode SelectedOperationMode { get; set; }
+    }
+
+    [Serializable]
+    public class HeaterParameters
+    {
+        public float SetTemperatureDay           { get; set; }
+        public float SetTemperatureNightLowering { get; set; }
+
+    }
+
+    public class HeaterControllerEventArgs : EventArgs
+    {
+        public bool TurnOn { get; set; }
+    }
+
+    public delegate void ActivityChanged( object sender, HeaterControllerEventArgs e );
+
+    public class HeaterController
+    {
+    }
+}
---
+++ HomeAutomationLibraries-master/HeaterController/INTERFACE/IHeaterControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeaterController.INTERFACE
+{
+    interface IHeaterControl
+    {
+        void Start();
+        void Pause();
+        void Resume();
+        void Stop();
+        event ActivityChanged EActivityChanged;
+
+    }
+}
