adebd98624bdef2603f1ef88c9e2355be12f765d
georgderflingerdeveloper <georgderflinger@gmail.com>
2017-10-06 21:35:01 +0200

    append

---
+++ HomeAutomationLibraries-master/TelegrEvaluator_UnitTests/TelegrammBroker_UnitTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using System;
+using TelegrammEvaluator;
+
+namespace TelegrEvaluator_UnitTests
+{
+    [TestFixture]
+    public class TelegrammBroker_UnitTests
+    {
+        TelegrammBrocker TestBroker;
+        object ExpectedObject;
+
+        void Setup( )
+        {
+            TestBroker = new TelegrammBrocker( new EvaluatorCollection() );
+        }
+
+        [Test]
+        public void TestBrockerOnWindowMessage()
+        {
+            Setup();
+            WindowState TestState = WindowState.eUnknown;
+            EvaluatorEventArgsWindow WindowArgs;
+
+            TestBroker.BEInformer += ( sender, e ) => 
+            {
+                ExpectedObject = sender;
+
+                if( ExpectedObject is WindowTelegrammEvaluator )
+                {
+                    WindowArgs = e as EvaluatorEventArgsWindow;
+                    TestState = WindowArgs.State;
+                }
+            };
+
+            TestBroker.ReceivedTelegramm = VerificationTelegramms.Telegramm_1;
+
+            Assert.AreEqual( WindowState.eAnyWindowIsOpen, TestState );
+        }
+
+        [Test]
+        public void TestBrockerOnDoorMessage()
+        {
+            Setup();
+            DoorState TestState = DoorState.eUnknown;
+            EvaluatorEventArgsDoor DoorArgs;
+
+            TestBroker.BEInformer += ( sender, e ) => 
+            {
+                ExpectedObject = sender;
+
+                if( ExpectedObject is DoorTelegrammEvaluator )
+                {
+                    DoorArgs = e as EvaluatorEventArgsDoor;
+                    TestState = DoorArgs.State;
+                }
+            };
+
+            TestBroker.ReceivedTelegramm = VerificationTelegramms.TelegrammDoor;
+
+            Assert.AreEqual( DoorState.eDoorIsOpen, TestState );
+        }
+
+    }
+}
---
+++ HomeAutomationLibraries-master/TelegrEvaluator_UnitTests/VerificationTelegramms.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TelegrEvaluator_UnitTests
+{
+    public static class VerificationTelegramms
+    {
+        public static string Telegramm_1        = "Schlafzimmer_FENSTERINFORMATION_FensterWestSeite_OFFEN_15052017:20h16m33s520ms_MansardenFensterLinkeSeite_GESCHLOSSEN_19122018:21h29m44s333ms";
+        public static string Telegramm_2        = "Schlafzimmer_FENSTERINFORMATION_FensterWestSeite_GESCHLOSSEN_19122018:21h29m44s333ms_MansardenFensterLinkeSeite_OFFEN_19122018:21h35m44s333ms";
+        public static string TelegrammAllClosed = "Schlafzimmer_FENSTERINFORMATION_FensterWestSeite_GESCHLOSSEN_19122018:21h29m44s333ms_MansardenFensterLinkeSeite_GESCHLOSSEN_19122018:21h35m44s333ms";
+        public static string TelegrammUnknown   = "Schlafzimmer_FENSTERINFORMATION_FensterWestSeite_UNBEKANNT_19122018:21h29m44s333ms_MansardenFensterLinkeSeite_OFFEN_19122018:21h35m44s333ms";
+        public static string TelegrammDoor      = "Wohnzimmer_TÜRINFORMATION_TürRechts_OFFEN_19122018:21h29m44s333ms";
+
+    }
+}
